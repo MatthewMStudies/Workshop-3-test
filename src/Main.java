cdd3bdd0229b8126a227493f1afa4a69b8885d6b
MatthewM <powermaf96@hotmail.com>
2025-08-07 13:40:57 +1000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+        System.out.printf("This is testing the Workshop 3 git test");
+        System.out.printf("Matthew signed");
+    }
+}
